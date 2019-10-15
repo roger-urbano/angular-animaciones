49d1b558cedd161d6453849e21abf3af08a90a6d
consultordev4@quimicasuiza.com <consultordev4@quimicasuiza.com>
2019-10-15 18:58:28 -0500

    configurando animacion de modals 70%

--- src/app/components/home/list-users/list-users.component.ts
+++ src/app/components/home/list-users/list-users.component.ts
@@ -1,9 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, style, transition, animate, state} from '@angular/animations';
 
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
-  styleUrls: ['./list-users.component.scss']
+  styleUrls: ['./list-users.component.scss'],
+    animations: [
+        trigger('starModal', [
+            state('void', style({
+                transform: 'translateY(200px)',
+                opacity: 0,
+                background: 'blue'
+            })),
+            transition(':enter', [
+                animate('300ms cubic-bezier(0.35, 0, 0.25, 1)', style({
+                    transform: 'translateY(0)',
+                    opacity: 1,
+                    background: 'yellow'
+                }))
+            ]),
+            transition(':leave',
+                animate('300ms cubic-bezier(0.35, 0, 0.25, 1)', style({
+                    transform: 'translateY(-300px)',
+                    opacity: 0,
+                    background: 'blue'
+                })))
+        ])
+    ]
 })
 export class ListUsersComponent implements OnInit {
 
@@ -21,8 +44,7 @@ export class ListUsersComponent implements OnInit {
   }
 
    closeModal($event: boolean) {
-      this.isOpenModal = $event;
-
+       this.isOpenModal = $event;
    }
 
 }
