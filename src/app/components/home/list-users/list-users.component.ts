922dcd29d15da0e27259af98700c70e1facbb684
consultordev4@quimicasuiza.com <consultordev4@quimicasuiza.com>
2019-10-17 18:17:21 -0500

    avancede animaction child, group

--- src/app/components/home/list-users/list-users.component.ts
+++ src/app/components/home/list-users/list-users.component.ts
@@ -1,40 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-import {trigger, style, transition, animate, state} from '@angular/animations';
+import {trigger, style, transition, animate, state, group, query, animateChild, stagger} from '@angular/animations';
 
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
   styleUrls: ['./list-users.component.scss'],
     animations: [
-        trigger('starModal', [
+        trigger('parentAnimation', [
             state('void', style({
-                transform: 'translateY(200px)',
-                opacity: 0,
-                background: 'blue'
+                // transform: 'translateY(100px)',
+                opacity: 0
             })),
             transition(':enter', [
-                animate('300ms cubic-bezier(0.35, 0, 0.25, 1)', style({
-                    transform: 'translateY(0)',
+                group([
+                    animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({
+                        // transform: 'translateY(0)',
+                        opacity: 1
+                    })),
+                    query('@childAnimation', stagger(100, [
+                        animateChild()
+                    ]))
+                ]),
+            ]),
+            transition(':leave', [
+                group([
+                    animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({
+                        // transform: 'translateY(-100px)',
+                        opacity: 0
+                    })),
+                    query('@childAnimation',[
+                        animateChild()
+                    ])
+                ]),
+            ]),
+
+        ]),
+        trigger('childAnimation', [
+            state('void', style({
+                transform: 'translateY(100px)',
+                opacity: 0
+            })),
+            transition(':enter', [
+                animate(500, style({
                     opacity: 1,
-                    background: 'yellow'
+                    transform: 'translateY(0)'
                 }))
             ]),
-            transition(':leave',
-                animate('300ms cubic-bezier(0.35, 0, 0.25, 1)', style({
-                    transform: 'translateY(-300px)',
+            transition(':leave', [
+                animate(500, style({
                     opacity: 0,
-                    background: 'blue'
-                })))
+                    transform: 'translateY(100px)'
+                }))
+            ])
         ])
     ]
 })
+
 export class ListUsersComponent implements OnInit {
 
   public isOpenModal: boolean;
+  public exp: boolean;
 
   constructor() { }
 
   ngOnInit() {
+      this.exp = false;
+      console.log(this.exp);
   }
 
   openModal() {
@@ -46,5 +77,10 @@ export class ListUsersComponent implements OnInit {
    closeModal($event: boolean) {
        this.isOpenModal = $event;
    }
+
+    changeColor() {
+      this.exp = !this.exp;
+      console.log(this.exp);
+    }
 
 }
--- src/app/modals/modal-alert/modal-alert.component.ts
+++ src/app/modals/modal-alert/modal-alert.component.ts
@@ -1,10 +1,29 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {trigger, style, transition, animate, state} from '@angular/animations';
 
 @Component({
   selector: 'modal-alert',
   templateUrl: './modal-alert.component.html',
-  styleUrls: ['./modal-alert.component.scss']
+  styleUrls: ['./modal-alert.component.scss'],
+
+    animations: [
+        trigger('animationModal', [
+            state('void', style({
+                transform: 'translateY(100px)',
+            })),
+            transition(':enter', [
+                animate('300ms cubic-bezier(0.35, 0, 0.25, 1)', style({
+                    transform: 'translateY(0)',
+                }))
+            ]),
+            transition(':leave',
+                animate('300ms cubic-bezier(0.35, 0, 0.25, 1)', style({
+                    transform: 'translateY(-100px)',
+                })))
+        ])
+    ]
 })
+
 export class ModalAlertComponent implements OnInit {
     @Input() public isOpenModal: boolean;
     @Output() public close: EventEmitter<boolean> = new EventEmitter();
